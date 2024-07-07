e632606b0b96bb812ecff5ae1e081c15e32d9ea9
spykelionel <social.lionel@gmail.com>
2024-07-08 00:15:18 +0100

    initial configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+# My ecommerce store
+
+## Installation
+
+- Clone project
+- Run `yarn` or `npm install` to install packages
---
+++ src/providers/AppProvider.jsx
@@ -0,0 +1,5 @@
+// import { router } from "@router";
+import { router } from "@router";
+import { RouterProvider } from "react-router-dom";
+
+export const AppProvider = () => <RouterProvider router={router} />;
