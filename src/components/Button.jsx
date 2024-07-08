6997f89915141d15d58358f0a47b72c71a06ca2f
spykelionel <social.lionel@gmail.com>
2024-07-08 06:20:23 +0100

    add optional prop to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
-export function Button({ title }) {
+export function Button({ title, lg }) {
   return (
-    <button className="bg-secondary hover:bg-yellow-600 text-white p-[0.25rem] rounded-lg text-sm">
+    <button
+      className={`bg-secondary hover:bg-yellow-600 text-white ${
+        lg ? "p-2" : "p-[0.25rem] "
+      } rounded-lg text-sm`}
+    >
       {title}
     </button>
   );
