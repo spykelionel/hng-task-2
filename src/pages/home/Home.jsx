8f6a519892f1fe17ca6d38ca8b7c1543b104371f
spykelionel <social.lionel@gmail.com>
2024-07-08 08:16:58 +0100

    adjust colors

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,9 +5,9 @@ export function Home() {
     "bg-[url(https://s3-alpha-sig.figma.com/img/6407/8abc/4069b8460a6c268b0a05b5b3828424f7?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dF9pqKPR3cOmXMlXsk3b7UcO~UIygqJ5PYmt7hbwsYNQOcO8RomGWiwr0SB-XrH8ib2YeyslEq-V86YqyBpdbPct1K~lFtwPguEPxrFBY6qDBibtUFzPUNgdXUOPvW-8~riQ38INNjOkmeU8ARKVRxHqio0ZQCMWqF92SV7H~HEyS6G2j08Nwbw9lGmiKQbYdoUgDM6mybmRF9WrJPN1iCliFOUsKV0dcgDEe4ukxKchS8fY32WqCuElo68tAYBQSSfunx7CJyWOmFZincGg~tvVuAgv9S17aX594Q0JnQ-OlunuCYXtOQzGZvj~0ADcXEk-hxrf2ZxZqG~4lrSEKQ__)]";
   return (
     <div className="">
-      <div className={`${bgString} py-2`}>
+      <div className={`bg-blur py-2`}>
         <div className="my-">
-          <div className="flex flex-col items-center gap-3 text-white sm:text-gray-900 my-2">
+          <div className="flex flex-col items-center gap-3 t sm:text-gray-900 my-2">
             <p className="font-bold text-xl">Shoes</p>
             <p className="">Explore Our range of footwears</p>
           </div>
