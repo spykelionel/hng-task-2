37da30971f9cd156d6fae3346e85db4ec7d48649
spykelionel <social.lionel@gmail.com>
2024-07-08 06:26:53 +0100

    update button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export function Button({ title, lg }) {
   return (
     <button
       className={`bg-secondary hover:bg-yellow-600 text-white ${
-        lg ? "p-2" : "p-[0.25rem] "
+        lg ? "p-3" : "p-[0.25rem] "
       } rounded-lg text-sm`}
     >
       {title}
