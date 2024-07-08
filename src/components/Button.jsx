c830dcbbdae2a71ef1bcd1a6caa3f12a25c18aeb
spykelionel <social.lionel@gmail.com>
2024-07-08 04:11:48 +0100

    add hover state to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({ title }) {
   return (
-    <button className="bg-secondary text-white p-[0.25rem] rounded-lg text-sm">
+    <button className="bg-secondary hover:bg-yellow-600 text-white p-[0.25rem] rounded-lg text-sm">
       {title}
     </button>
   );
