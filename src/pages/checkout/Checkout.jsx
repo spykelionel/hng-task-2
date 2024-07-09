382d84dff84776a444608f117f891eaa98be9e44
spykelionel <social.lionel@gmail.com>
2024-07-09 19:27:12 +0100

    fix checkout

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -177,20 +177,68 @@ export function Checkout() {
             <Button title={"Continue"} lg />
           </div>
         </div>
-        <div className="flex my-2 border border-solid w-1/2 p-2">
-          <div className="flex flex-col">
-            <div className="flex flex-col gap-4 border-b-2 border-solid border-gray-300 my-2 py-2">
-              <div className="flex gap-5 justify-between">
+        <div className="flex flex-1 self-start justify-center items-center m-2 border border-solid rounded-md w-full md:w-1/2 h-fit md:h-fit p-5">
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-col justify-center gap-4 border-b-2 border-solid border-gray-300 my-2 py-2">
+              <div className="flex gap-x010 justify-between w-full">
                 <p>Price</p>
-                <p>N 40, 000</p>
+                <p className="flex items-center gap-1">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="15"
+                      viewBox="0 0 12 15"
+                      fill="none"
+                    >
+                      <path
+                        d="M0 5.25H1.5V0.75H3L5.565 5.25H9V0.75H10.5V5.25H12V6.75H10.5V8.25H12V9.75H10.5V14.25H9L6.4275 9.75H3V14.25H1.5V9.75H0V8.25H1.5V6.75H0V5.25ZM3 5.25H3.8475L3 3.7725V5.25ZM3 6.75V8.25H5.565L4.71 6.75H3ZM9 11.25V9.75H8.1375L9 11.25ZM6.42 6.75L7.2825 8.25H9V6.75H6.42Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </span>
+                  <span>242, 00</span>
+                </p>
               </div>
               <div className="flex gap-5 justify-between">
                 <p>Discount</p>
-                <p>N 0.00</p>
+                <p className="flex items-center gap-1">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="15"
+                      viewBox="0 0 12 15"
+                      fill="none"
+                    >
+                      <path
+                        d="M0 5.25H1.5V0.75H3L5.565 5.25H9V0.75H10.5V5.25H12V6.75H10.5V8.25H12V9.75H10.5V14.25H9L6.4275 9.75H3V14.25H1.5V9.75H0V8.25H1.5V6.75H0V5.25ZM3 5.25H3.8475L3 3.7725V5.25ZM3 6.75V8.25H5.565L4.71 6.75H3ZM9 11.25V9.75H8.1375L9 11.25ZM6.42 6.75L7.2825 8.25H9V6.75H6.42Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </span>
+                  <span className="text-nowrap">242, 00</span>
+                </p>
               </div>
-              <div className="flex gap-5 justify-between">
+              <div className="flex gap-5 justify-between ">
                 <p>V.A.T</p>
-                <p>N 0.00</p>
+                <p className="flex items-center gap-1">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="15"
+                      viewBox="0 0 12 15"
+                      fill="none"
+                    >
+                      <path
+                        d="M0 5.25H1.5V0.75H3L5.565 5.25H9V0.75H10.5V5.25H12V6.75H10.5V8.25H12V9.75H10.5V14.25H9L6.4275 9.75H3V14.25H1.5V9.75H0V8.25H1.5V6.75H0V5.25ZM3 5.25H3.8475L3 3.7725V5.25ZM3 6.75V8.25H5.565L4.71 6.75H3ZM9 11.25V9.75H8.1375L9 11.25ZM6.42 6.75L7.2825 8.25H9V6.75H6.42Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </span>
+                  <span className="text-nowrap">0.00</span>
+                </p>
               </div>
               <div className="flex gap-5 justify-between">
                 <p>Shipping</p>
